bb108dd323ae39ef36ad2783d34fd719abb260b9
Harpreetd <dhimanharpreet17@yahoo.in>
2022-02-06 15:40:59 +0100

    oppgave 2 ferdig

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+// created a class
 class Animal {
-  constructor(name) {
+  constructor(species, name, country, color, imgUrl) {
     this.species = species;
     this.name = name;
     this.country = country;
     this.color = color;
     this.imgUrl = imgUrl;
-  }
+    }
+    
   feedAnimal() {}
 }
+// calling the constructor function tp craete the first object with the following values
+let firstAnimal = new Animal(
+  "Cat-Family",
+  "Cheetah",
+  "Iran",
+  "yellow",
+  "https://pixabay.com/no/photos/gepard-dyr-dyreliv-pattedyr-3749168/"
+);
+// calling the constructor function to create second object
+let secondAnimal = new Animal(
+  "Bird",
+  "Penguin",
+  "Antarctica",
+  "Black and white",
+  "https://pixabay.com/no/photos/antarktis-penguin-pingvin-dyr-sn%c3%b8-3427135/"
+);
 
-let firstAnimal = new Animal("Cat");
-let secondAnimal = new Animal("Bird");
+// console logging the values from the two objects craeted above
+console.log(firstAnimal.name);
+console.log(secondAnimal.color);
+console.log(firstAnimal.species);
+console.log(secondAnimal.imgUrl);
+console.log(firstAnimal.country);
