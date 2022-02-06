d17a1003324ca7c00c2feb17111dd2535a19d6f0
Harpreetd <dhimanharpreet17@yahoo.in>
2022-02-06 15:12:33 +0100

    created a class Animal with two objects Cat and Bird

---
+++ index.js
@@ -0,0 +1,13 @@
+class Animal {
+  constructor(name) {
+    this.species = species;
+    this.name = name;
+    this.country = country;
+    this.color = color;
+    this.imgUrl = imgUrl;
+  }
+  feedAnimal() {}
+}
+
+let firstAnimal = new Animal("Cat");
+let secondAnimal = new Animal("Bird");
